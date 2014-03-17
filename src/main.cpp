03446d4dbcd72a057ff4dbfc3d1683c6764f1ae4
Adam Eliot <adam@Hephaestus>
2014-03-16 20:52:39 -0400

    Trying with time delays

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include "robotSerial.hpp"
+#include <boost/thread/thread.hpp>
 
 int main( int argc, char** argv){
     RobotSerial robot;
+    boost::this_thread::sleep(boost::posix_time::milliseconds(100));
     robot.start();
+    boost::this_thread::sleep(boost::posix_time::milliseconds(100));
     robot.full();
 }
