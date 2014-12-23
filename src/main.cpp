96d491aa04b18d348b0c99f24d5f2c5c34479c5f
Adam Eliot <adameliot@gmail.com>
2014-12-23 16:07:58 -0500

    Added some logging

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,126 @@
 #include "robotSerial.hpp"
 #include <boost/thread/thread.hpp>
+#include <syslog.h>
+#include <map>
 
-int main( int argc, char** argv){
-    // RobotSerial robot("/dev/ttyAMA0");
-    RobotSerial robot("/dev/ttyUSB0");
+using namespace std;
+
+enum class Args : int
+{
+    SRV_PORT,
+    TTY
+};
+
+
+enum class Opts : int
+{
+    DEBUG,
+    VERB
+};
+
+
+void printHelp()
+{
+    cout << "PiRobot" << endl;
+    cout << "=======" << endl;
+    cout << "Flag          Type      Default           Description     " << endl;
+    cout << "----------------------------------------------------------" << endl;
+    cout << "-p, --port    [int]     12000             TCP Server Port." << endl;
+    cout << "-t, --tty     [string]  /dev/ttyAMA0      Serial port.    " << endl;
+    cout << "-d, --debug                               Log debug level." << endl;
+    cout << "-v, --verbose                             Log to stdout.  " << endl;
+    cout << "-h, --help                                Print help.     " << endl;
+}
+
+
+void parsArgs(int argc, char** argv, map<Args, string>& args, 
+             map<Opts, bool>& opts)
+{
+    args[Args::SRV_PORT] = "12000";
+    args[Args::TTY] = "/dev/ttyAMA0";
+
+    opts[Opts::DEBUG] = false;
+    opts[Opts::VERB] = false;
+
+    int i = 0;
+    while(i < argc)
+    {
+        if(argv[i] == "-p" || argv[i] == "--port")
+        {
+            i++;
+            if(i => argc)
+            {
+                cerr << "ERROR: Bad argument." << endl;
+                printHelp();
+                exit(-1);
+            }
+            args[Args::SRV_PORT] = argv[i];
+            i++;
+        }
+        else if(argv[i] == "-t" || argv[i] == "--tty")
+        {
+            i++;
+            if(i => argc)
+            {
+                cerr << "ERROR: Bad argument." << endl;
+                printHelp();
+                exit(-1);
+            }
+            args[Args::TTY] == argv[i];
+            i++;
+        }
+        else if(argv[i] == "-d" || argv[i] == "--debug")
+        {
+            opts[Opts::DEBUG] = true;
+            i++;
+        }
+        else if(argv[i] == "-v" || argv[i] == "--verbose")
+        {
+            opts[Opts::VERB] = true;
+        }
+        else if(argv[i] == "-h" || argv[i] == "--help")
+        {
+            printHelp();
+            exit(0);
+        }
+        else
+        {
+            cerr << "ERROR: Unknown argument." << endl;
+            printHelp();
+            exit(-1);
+        }
+    }   
+}
+
+
+int main( int argc, char** argv)
+{
+    map<Args, string> args;
+    map<Opts, string> opts;
+    parsArgs(argc, argv, args, opts);
+
+    if(opts[Opts::DEBUG])
+    {
+        setlogmask(LOG_UPTO(LOG_DEBUG));
+    }
+    else
+    {
+        setlogmask(LOG_UPTO(LOG_NOTICE));
+    }
+
+    int logOpts = LOG_CONS | LOG_PID | LOG_NDELAY;
+    if(opts[Opts::VERB])
+    {
+        logOpts |= LOG_PERROR;
+    }
+
+    openlog("PiRobot", logOpts, LOG_LOCAL1);
+
+    syslog(LOG_NOTICE, "Starting serial communication. TTY[%s]", args[Args::TTY]);
+    RobotSerial robot(args[Args::TTY]);
+
     boost::this_thread::sleep(boost::posix_time::milliseconds(100));
+    syslog(LOG_DEBUG, "Sending start signal.");
     robot.start();
     boost::this_thread::sleep(boost::posix_time::milliseconds(100));
     robot.full();
@@ -18,4 +134,5 @@ int main( int argc, char** argv){
     robot.drive(0,0);
 
     boost::this_thread::sleep(boost::posix_time::milliseconds(100));
+    closelog();
 }
--- src/robotSerial.cpp
+++ src/robotSerial.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <stdexcept>
+#include <syslog.h>
 
 using namespace std;
 using namespace boost;
@@ -20,10 +21,13 @@ RobotSerial::RobotSerial(string serialPath)
     ioSrv = new io_service();
     serPort = new serial_port(*ioSrv, serialPath);
 
-    if(!serPort->is_open()){
+    if(!serPort->is_open())
+    {
+        syslog(LOG_ERR, "Unable to open serial port. TTY [%s]", serialPath);
         throw runtime_error("Unable to open serial port.");
     }
 
+    syslog(LOG_DEBUG, "Setting baud rate. Baud [%d]", DEFAULT_BAUD);
     serial_port_base::baud_rate baud_opt(DEFAULT_BAUD);
     serPort->set_option(baud_opt);
 }
@@ -39,6 +43,7 @@ RobotSerial::~RobotSerial()
 //System Controls
 void RobotSerial::start()
 {
+    syslog(LOG_DEBUG, "Sending start signal.");
     unsigned char msg[1];
     msg[0] = OPCODE_START;
     write(*serPort, buffer(msg, 1));
@@ -47,6 +52,7 @@ void RobotSerial::start()
 
 void RobotSerial::full()
 {
+    syslog(LOG_DEBUG, "Sending full control signal.");
     unsigned char msg[1];
     msg[0] = OPCODE_FULL;
     write(*serPort, buffer(msg, 1));
@@ -55,10 +61,13 @@ void RobotSerial::full()
 
 void RobotSerial::baud(unsigned char bps)
 {
-    if(bps > 11){
+    if(bps > 11)
+    {
+        syslog(LOG_ERR, "Invalid baud provided. Baud [%d]", bps);
         throw runtime_error("Invalid baud rate.");
     }
 
+    syslog(LOG_NOTICE, "Changing baud rate. Baud [%d]", bps);
     unsigned char msg[2];
     msg[0] = OPCODE_BAUD;
     msg[1] = bps;
@@ -69,6 +78,7 @@ void RobotSerial::baud(unsigned char bps)
 
 void RobotSerial::safe()
 {
+    syslog(LOG_DEBUG, "Sending safe mode signal.");
     unsigned char msg[1];
     msg[0] = OPCODE_SAFE;
     write(*serPort, buffer(msg, 1));
@@ -78,10 +88,13 @@ void RobotSerial::safe()
 //Demos
 void RobotSerial::demo(unsigned char mode)
 {
-    if(mode != 255 && mode > 9){
+    if(mode != 255 && mode > 9)
+    {
+        syslog(LOG_ERR, "Invalid demo number. Demo [%d]", mode);
         throw runtime_error("Invalid demo number.");
     }
 
+    syslog(LOG_DEBUG, "Sending demo mode signal. Demo [%d]", mode);
     unsigned char msg[2];
     msg[0] = OPCODE_DEMO;
     msg[1] = mode;
@@ -91,6 +104,7 @@ void RobotSerial::demo(unsigned char mode)
 
 void RobotSerial::cover()
 {
+    syslog(LOG_DEBUG, "Sending cover signal.");
     unsigned char msg[1];
     msg[0] = OPCODE_COVER;
     write(*serPort, buffer(msg, 1));
@@ -99,13 +113,16 @@ void RobotSerial::cover()
 
 void RobotSerial::coverDock()
 {
+    syslog(LOG_DEBUG, "Sending cover dock signal.");
     unsigned char msg[1];
     msg[0] = OPCODE_COVER_DOCK;
     write(*serPort, buffer(msg, 1));
 }
 
+
 void RobotSerial::spot()
 {
+    syslog(LOG_DEBUG, "Sending spot signal.");
     unsigned char msg[1];
     msg[0] = OPCODE_SPOT;
     write(*serPort, buffer(msg, 1));
@@ -113,17 +130,24 @@ void RobotSerial::spot()
 
 
 //Actuator
-void RobotSerial::drive(int vel, int rad){
-    if(vel < -500 || vel > 500){
+void RobotSerial::drive(int vel, int rad)
+{
+    if(vel < -500 || vel > 500)
+    {
+        syslog(LOG_ERR, "Invalid velocity. Velocity [%d]", vel);
         throw runtime_error("Invalid velocity.");
     }
+
     if((rad < -2000 || rad > 2000) &&
         rad != DRIVE_STRAIGHT &&
         rad != DRIVE_TURN_RIGHT &&
-        rad != DRIVE_TURN_LEFT){
-
+        rad != DRIVE_TURN_LEFT)
+    {
+        syslog(LOG_ERR, "Invalid turn radius. Radius [%d]", rad);
         throw runtime_error("Invalid turn radius.");
     }
+
+    syslog(LOG_NOTICE, "Sending drive signal. Velocity [%d] Radius [%d]", vel, rad);
 
     this->drive((unsigned char)((vel >> 8)&0xFF),
                 (unsigned char)(vel & 0xFF),
@@ -131,8 +155,13 @@ void RobotSerial::drive(int vel, int rad){
                 (unsigned char)(rad & 0xFF));
 }
 
+
 void RobotSerial::drive(unsigned char velH, unsigned char velL,
-                   unsigned char radH, unsigned char radL){
+                        unsigned char radH, unsigned char radL)
+{
+    syslog(LOG_DEBUG, "Sending drive signal. VelH [%d] VelL [%d] RadH [%d] RadL [%d]",
+           velH, velL, radH, radL);
+
     unsigned char msg[5];
     msg[0] = OPCODE_DRIVE;
     msg[1] = velH;
@@ -143,13 +172,22 @@ void RobotSerial::drive(unsigned char velH, unsigned char velL,
     write(*serPort, buffer(msg, 5));
 }
 
-void RobotSerial::driveDirect(int left, int right){
-    if(left > 500 || left < -500){
+
+void RobotSerial::driveDirect(int left, int right)
+{
+    if(left > 500 || left < -500)
+    {
+        syslog(LOG_ERR, "Invalid left velocity. Velocity [%d]", left);
         throw runtime_error("Invalid left velocity.");
     }
-    if(right > 500 || right < -500){
+
+    if(right > 500 || right < -500)
+    {
+        syslog(LOG_ERR, "Invalid right velocity. Velocity [%d]", right);
         throw runtime_error("Invalid right velocity.");
     }
+
+    syslog(LOG_NOTICE, "Sending drive signal. Left [%d] Right [%d]", left, right);
 
     this->drive((unsigned char)((left >> 8)&0xFF),
                 (unsigned char)(left&0xFF),
@@ -157,8 +195,13 @@ void RobotSerial::driveDirect(int left, int right){
                 (unsigned char)(right&0xFF));
 }
 
+
 void RobotSerial::driveDirect(unsigned char leftH, unsigned char leftL,
-                         unsigned char rightH, unsigned char rightL){
+                         unsigned char rightH, unsigned char rightL)
+{
+    syslog(LOG_DEBUG, "Sending drive signal. LeftH [%d] LeftL [%d] RightH [%d] RightL [%d]",
+           leftH, leftL, rightH, rightL);
+
     unsigned char msg[5];
     msg[0] = OPCODE_DRIVE_DIRECT;
     msg[1] = rightH;
@@ -167,11 +210,15 @@ void RobotSerial::driveDirect(unsigned char leftH, unsigned char leftL,
     msg[4] = leftL;
 
     write(*serPort, buffer(msg, 5));
-
 }
 
+
 void RobotSerial::led(unsigned char ledMask, unsigned char pColor,
-                 unsigned char pIntensity){
+                 unsigned char pIntensity)
+{
+    syslog(LOG_DEBUG, "Sending led signal. Mask [%#x] Color [%d] Intensity [%d]",
+           ledMask, pColor, pIntensity);
+
     unsigned char msg[4];
     msg[0] = OPCODE_LEDS;
     msg[1] = ledMask;
@@ -181,23 +228,29 @@ void RobotSerial::led(unsigned char ledMask, unsigned char pColor,
     write(*serPort, buffer(msg, 4));
 }
 
-void RobotSerial::led(bool advance, bool play, unsigned char pColor,
-                 unsigned char pIntensity){
 
+void RobotSerial::led(bool advance, bool play, unsigned char pColor,
+                 unsigned char pIntensity)
+{
     unsigned char ledMask = 0;
 
-    if (advance){
+    if (advance)
+    {
         ledMask |= LED_ADVANCE;
     }
 
-    if (play){
+    if (play)
+    {
         ledMask |= LED_PLAY;
     }
 
     this->led(ledMask, pColor, pIntensity);
 }
 
-void RobotSerial::digitalOut(unsigned char outBits){
+
+void RobotSerial::digitalOut(unsigned char outBits)
+{
+    syslog(LOG_DEBUG, "Sending digital output signal. Out [%#x]", outBits);
     unsigned char msg[2];
     msg[0] = OPCODE_DIGITAL_OUT;
     msg[1] = outBits;
@@ -205,31 +258,44 @@ void RobotSerial::digitalOut(unsigned char outBits){
     write(*serPort, buffer(msg, 2));
 }
 
-void RobotSerial::digitalOut(bool outA, bool outB, bool outC){
+
+void RobotSerial::digitalOut(bool outA, bool outB, bool outC)
+{
     unsigned char outMask = 0;
 
-    if (outA){
+    if (outA)
+    {
         outMask |= 1;
     }
 
-    if (outB){
+    if (outB)
+    {
         outMask |= 2;
     }
 
-    if (outC){
+    if (outC)
+    {
         outMask |= 4;
     }
 
     this->digitalOut(outMask);
 }
 
+
 void RobotSerial::lowSideDrivePWM(unsigned char driveA, unsigned char driveB,
-                             unsigned char driveC){
+                                  unsigned char driveC)
+{
     unsigned char msg[4];
 
-    if(driveA > 128 || driveB > 128 || driveC > 128){
+    if(driveA > 128 || driveB > 128 || driveC > 128)
+    {
+        syslog(LOG_ERR, "Invalid low side drive value. DriveA [%d] DriveB [%d] DriveC [%d]",
+               driveA, driveB, driveC);
         throw runtime_error("Invalid PWM duty cycle. Must be 0-128.");
     }
+
+    syslog(LOG_DEBUG, "Sending low side drive PWM signal. DriveA [%d] DriveB [%d] DriveC[%d]",
+           driveA, driveB, driveC);
 
     msg[0] = OPCODE_LOW_SIDE_PWM;
     msg[1] = driveC;
@@ -239,8 +305,12 @@ void RobotSerial::lowSideDrivePWM(unsigned char driveA, unsigned char driveB,
     write(*serPort, buffer(msg, 4));
 }
 
-void RobotSerial::lowSideDrive(unsigned char drives){
+
+void RobotSerial::lowSideDrive(unsigned char drives)
+{
     unsigned char msg[2];
+
+    syslog(LOG_DEBUG, "Sending low side drive signal. Drives [%#x]", drives);
 
     msg[0] = OPCODE_LOW_SIDE;
     msg[1] = drives;
@@ -248,26 +318,35 @@ void RobotSerial::lowSideDrive(unsigned char drives){
     write(*serPort, buffer(msg, 2));
 }
 
-void RobotSerial::lowSideDrive(bool driveA, bool driveB, bool driveC){
+
+void RobotSerial::lowSideDrive(bool driveA, bool driveB, bool driveC)
+{
     unsigned char driveMask = 0;
 
-    if (driveA){
+    if (driveA)
+    {
         driveMask |= 1;
     }
 
-    if (driveB){
+    if (driveB)
+    {
         driveMask |= 2;
     }
 
-    if (driveC){
+    if (driveC)
+    {
         driveMask |= 4;
     }
 
     this->lowSideDrive(driveMask);
 }
 
-void RobotSerial::sendIR(unsigned char value){
+
+void RobotSerial::sendIR(unsigned char value)
+{
     unsigned char msg[2];
+
+    syslog(LOG_DEBUG, "Sending IR signal. Value [%#x]", value);
 
     msg[0] = OPCODE_SEND_IR;
     msg[1] = value;
@@ -275,30 +354,37 @@ void RobotSerial::sendIR(unsigned char value){
     write(*serPort, buffer(msg, 2));
 }
 
-void RobotSerial::song(unsigned char id, vector<Note> notes){
 
-    if(id > 15){
+void RobotSerial::song(unsigned char id, vector<Note> notes)
+{
+    if(id > 15)
+    {
+        syslog(LOG_ERR, "Invalid song number. Song [%d]", id);
         throw runtime_error("Invalid song number. Must be 0-15.");
     }
 
-    if(notes.size() > 16 || notes.size() == 0){
+    if(notes.size() > 16 || notes.size() == 0)
+    {
+        syslog(LOG_ERR, "Invalid number of notes. Length [%d]", notes.size());
         throw runtime_error("Invalid number of notes. Must be 1-16.");
     }
 
     unsigned int length = (notes.size()<<1) + 3;
-
     unsigned char* msg;
     msg = (unsigned char*)malloc(length);
 
     unsigned int i = 0;
+
+    syslog(LOG_DEBUG, "Sending song signal. Song [%d] Length [%d]", 
+           id, notes.size());
 
     msg[i++] = OPCODE_SONG;
     msg[i++] = id;
     msg[i++] = notes.size();
 
     for(vector<Note>::iterator it = notes.begin();
-        it != notes.end(); it++){
-
+        it != notes.end(); it++)
+    {
         msg[i++] = it->number;
         msg[i++] = it->length;
     }
@@ -307,12 +393,18 @@ void RobotSerial::song(unsigned char id, vector<Note> notes){
     free(msg);
 }
 
-void RobotSerial::playSong(unsigned char id){
-    if(id > 15){
+
+void RobotSerial::playSong(unsigned char id)
+{
+    if(id > 15)
+    {
+        syslog(LOG_ERR, "Invalid song number. Song [%d]", id);
         throw runtime_error("Invalid song number. Must be 0-15.");
     }
 
     unsigned char msg[2];
+
+    syslog(LOG_DEBUG, "Sending play song signal. Song [%d]", id);
 
     msg[0] = OPCODE_SONG_PLAY;
     msg[1] = id;
@@ -322,17 +414,21 @@ void RobotSerial::playSong(unsigned char id){
 
 
 //Inputs
-vector<unsigned char> RobotSerial::sensors(unsigned char id){
-
+vector<unsigned char> RobotSerial::sensors(unsigned char id)
+{
     vector<unsigned char> data;
+    //TODO
     return data;
 }
 
-vector<unsigned char> RobotSerial::queryList(vector<unsigned char> list){
 
+vector<unsigned char> RobotSerial::queryList(vector<unsigned char> list)
+{
     vector<unsigned char> data;
+    //TODO
     return data;
 }
+
 
 //TODO:
 ///stream
@@ -340,42 +436,58 @@ vector<unsigned char> RobotSerial::queryList(vector<unsigned char> list){
 
 
 //Script
-void RobotSerial::script(vector<unsigned char> opcodes){
-
+void RobotSerial::script(vector<unsigned char> opcodes)
+{
+    //TODO
 }
 
-void RobotSerial::playScript(){
 
+void RobotSerial::playScript()
+{
+    //TODO
 }
 
-vector<unsigned char> RobotSerial::showScript(){
 
+vector<unsigned char> RobotSerial::showScript()
+{
     vector<unsigned char> data;
+    //TODO
     return data;
 }
 
 
 //Wait commands
-void RobotSerial::waitTime(unsigned char time){
-
+void RobotSerial::waitTime(unsigned char time)
+{
+    //TODO
 }
 
-void RobotSerial::waitDistance(int dist){
 
+void RobotSerial::waitDistance(int dist)
+{
+    //TODO
 }
 
-void RobotSerial::waitDistance(unsigned char distH, unsigned char distL){
 
+void RobotSerial::waitDistance(unsigned char distH, unsigned char distL)
+{
+    //TODO
 }
 
-void RobotSerial::waitAngle(int ang){
 
+void RobotSerial::waitAngle(int ang)
+{
+    //TODO
 }
 
-void RobotSerial::waitAngle(unsigned char angH, unsigned char angL){
 
+void RobotSerial::waitAngle(unsigned char angH, unsigned char angL)
+{
+    //TODO
 }
 
-void RobotSerial::waitEvent(unsigned char event){
 
+void RobotSerial::waitEvent(unsigned char event)
+{
+    //TODO
 }
